76a68d1a02b6fa31d3be777365a0732095c9e99e
Kris van Rens <krisvanrens@gmail.com>
2022-05-01 23:40:54 +0200

    CHG: Extend scanner to ending of chapter 4.

--- jlox/com/craftinginterpreters/lox/Scanner.java
+++ jlox/com/craftinginterpreters/lox/Scanner.java
@@ -14,6 +14,28 @@ class Scanner {
   private int current = 0;
   private int line = 1;
 
+  private static final Map<String, TokenType> keywords;
+
+  static {
+    keywords = new HashMap<>();
+    keywords.put("and",    AND);
+    keywords.put("class",  CLASS);
+    keywords.put("else",   ELSE);
+    keywords.put("false",  FALSE);
+    keywords.put("for",    FOR);
+    keywords.put("fun",    FUN);
+    keywords.put("if",     IF);
+    keywords.put("nil",    NIL);
+    keywords.put("or",     OR);
+    keywords.put("print",  PRINT);
+    keywords.put("return", RETURN);
+    keywords.put("super",  SUPER);
+    keywords.put("this",   THIS);
+    keywords.put("true",   TRUE);
+    keywords.put("var",    VAR);
+    keywords.put("while",  WHILE);
+  }
+
   Scanner(String source) {
     this.source = source;
   }
@@ -80,9 +102,46 @@ class Scanner {
         break;
 
       default:
-        Lox.error(line, "Unexpected character.");
+        if (isDigit(c)) {
+          number();
+        } else if (isAlpha(c)) {
+          identifier();
+        } else {
+          Lox.error(line, "Unexpected character.");
+        }
         break;
     }
+  }
+
+  private void identifier() {
+    while (isAlphaNumeric(peek())) {
+      advance();
+    }
+
+    String text = source.substring(start, current);
+    TokenType type = keywords.get(text);
+    if (type == null) {
+      type = IDENTIFIER;
+    }
+
+    addToken(type);
+  }
+
+  private void number() {
+    while (isDigit(peek())) {
+      advance();
+    }
+
+    // Look for a fractional part.
+    if ((peek() == '.') && isDigit(peekNext())) {
+      advance(); // Consume the dot.
+
+      while (isDigit(peek())) {
+        advance();
+      }
+    }
+
+    addToken(NUMBER, Double.parseDouble(source.substring(start, current)));
   }
 
   private void string() {
@@ -124,6 +183,28 @@ class Scanner {
     }
 
     return source.charAt(current);
+  }
+
+  private char peekNext() {
+    if ((current + 1) >= source.length()) {
+      return '\0';
+    }
+
+    return source.charAt(current + 1);
+  }
+
+  private boolean isAlpha(char c) {
+    return ((c >= 'a') && (c <= 'z')) ||
+           ((c >= 'A') && (c <= 'Z')) ||
+            (c == '_');
+  }
+
+  private boolean isAlphaNumeric(char c) {
+    return isAlpha(c) || isDigit(c);
+  }
+
+  private boolean isDigit(char c) {
+    return ((c >= '0') && (c <= '9'));
   }
 
   private boolean isAtEnd() {
