a24c856705f6aa1ae0c18f6b2fc02f140a2e8b02
Kris van Rens <krisvanrens@gmail.com>
2022-05-11 21:28:03 +0200

    ADD: Statement parsing and interpretation.

--- jlox/com/craftinginterpreters/lox/Lox.java
+++ jlox/com/craftinginterpreters/lox/Lox.java
@@ -61,15 +61,13 @@ public class Lox {
     List<Token> tokens = scanner.scanTokens();
 
     Parser parser = new Parser(tokens);
-    Expr expression = parser.parse();
+    List<Stmt> statements = parser.parse();
 
     if (hadError) {
       return;
     }
 
-    interpreter.interpret(expression);
-
-    System.out.println(new AstPrinter().print(expression));
+    interpreter.interpret(statements);
   }
 
   static void error(int line, String message) {
