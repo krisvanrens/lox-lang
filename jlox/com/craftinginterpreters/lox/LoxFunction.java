32c329729a2ebbdf6053aaba86fd98c2f681f386
Kris van Rens <krisvanrens@gmail.com>
2022-05-29 22:38:29 +0200

    ADD: Class initializers.

--- jlox/com/craftinginterpreters/lox/LoxFunction.java
+++ jlox/com/craftinginterpreters/lox/LoxFunction.java
@@ -5,17 +5,19 @@ import java.util.List;
 class LoxFunction implements LoxCallable {
   private final Stmt.Function declaration;
   private final Environment closure;
+  private final Boolean isInitializer;
 
-  LoxFunction(Stmt.Function declaration, Environment closure) {
+  LoxFunction(Stmt.Function declaration, Environment closure, Boolean isInitializer) {
     this.declaration = declaration;
     this.closure = closure;
+    this.isInitializer = isInitializer;
   }
 
   LoxFunction bind(LoxInstance instance) {
     Environment environment = new Environment(closure);
     environment.define("this", instance);
 
-    return new LoxFunction(declaration, environment);
+    return new LoxFunction(declaration, environment, isInitializer);
   }
 
   @Override
@@ -38,7 +40,15 @@ class LoxFunction implements LoxCallable {
     try {
       interpreter.executeBlock(declaration.body, environment);
     } catch (Return returnValue) {
+      if (isInitializer) {
+        return closure.getAt(0, "this");
+      }
+
       return returnValue.value;
+    }
+
+    if (isInitializer) {
+      return closure.getAt(0, "this");
     }
 
     return null;
--- jlox/com/craftinginterpreters/lox/Resolver.java
+++ jlox/com/craftinginterpreters/lox/Resolver.java
@@ -17,6 +17,7 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
   private enum FunctionType {
     NONE,
     FUNCTION,
+    INITIALIZER,
     METHOD
   }
 
@@ -49,6 +50,11 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
     for (Stmt.Function method : stmt.methods) {
       FunctionType declaration = FunctionType.METHOD;
+
+      if (method.name.lexeme.equals("init")) {
+        declaration = FunctionType.INITIALIZER;
+      }
+
       resolveFunction(method, declaration);
     }
 
@@ -100,6 +106,10 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
     }
 
     if (stmt.value != null) {
+      if (currentFunction == FunctionType.INITIALIZER) {
+        Lox.error(stmt.keyword, "Cannot return an error from an initializer.");
+      }
+
       resolve(stmt.value);
     }
 
